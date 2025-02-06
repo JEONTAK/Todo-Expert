24558f271ebf2060c6f92eead441ad480c763fc0
JEONTAK <tak980418@naver.com>
2025-02-06 10:50:48 +0900

    Initial commit

---
+++ src/test/java/com/example/todoexpert/TodoExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoExpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
